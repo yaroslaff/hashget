fc2584bac3a0710f358b2888b8f4bc4e4ab1c1de
Yaroslav Polyakov <yaroslaff@gmail.com>
2019-02-19 23:29:24 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='hashget',
+    version='0.9',
+    packages=['hashget'],
+    scripts=['bin/hashget', 'bin/hashget-admin'],
+
+    install_requires=['filelock'],
+
+    url='https://gitlab.com/yaroslaff/hashget',
+    license='MIT',
+    author='Yaroslav Polyakov',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author_email='yaroslaff@gmail.com',
+    description='hashget deduplication and compression tool'
+)
