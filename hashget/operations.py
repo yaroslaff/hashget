d5f406b152a0f3f68ff4c3c45d52e6db5362147d
Yaroslav <yaroslaff@gmail.com>
2021-04-07 17:54:10 +0700

    fixed debug print

--- hashget/operations.py
+++ hashget/operations.py
@@ -67,7 +67,7 @@ def index(hashdb, root, anchors = None, filesz=None, heuristics=None, pool=None,
                         sr.submit(pool=pool, project=project)
                     except (DownloadFailure, BrokenPackage) as e:
                         c.inc('failed')
-                        log.error("ERROR processing".format(sr.url))
+                        log.error("ERROR processing URL {}".format(sr.url))
                     else:
                         c.inc('new')
                 else:
